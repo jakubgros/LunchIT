6a843f7a09a811c772357ac4dc09be8d35c17029
Jakub Gros <kubagros@gmail.com>
2019-10-05 17:35:56 +0200

    added FoodMenusBar

---
+++ src/lunch_it/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+        builder: (context, orientation) {
+          return orientation == Orientation.portrait
+              ? VerticalLayout()
+              : HorizontalLayout();
+        });
+    }
+  }
+
+class HorizontalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Text("Not implemented")//TODO implement
+        )
+    );
+  }
+}
+
+class VerticalLayout extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("#MAIL TITLE#"), //TODO
+              leading: BackButton(),
+            ),
+            body: FoodMenusBar(10) //TODO unmock
+        )
+    );
+  }
+}
+
+class FoodMenusBar extends StatelessWidget {
+  final _amountOfEntries;
+  final _menuFractionalRelativeSize = 1/15;
+
+  FoodMenusBar(this._amountOfEntries);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.blue[300],
+      child: FractionallySizedBox(
+        heightFactor: _menuFractionalRelativeSize,
+        child: ListView.separated(
+          separatorBuilder: (context, index) => VerticalDivider(
+            color: Colors.black,
+          ),
+          itemCount: _amountOfEntries,
+
+          scrollDirection: Axis.horizontal,
+          itemBuilder: FoodMenusBarEntryBuilder,
+        ),
+      ),
+    );
+  }
+}
+
+Widget FoodMenusBarEntryBuilder(BuildContext context, int index) //TODO implement
+{
+  String title = "Menu $index";
+  return InkWell(
+    onTap: () {;},
+    child: FractionallySizedBox(
+      heightFactor: 1/2,
+      child: FittedBox(
+        fit: BoxFit.fill,
+        child: Center(
+            child: Text(title)
+        ),
+      ),
+    ),
+  );
+
+}

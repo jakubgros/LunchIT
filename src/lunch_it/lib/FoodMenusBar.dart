deb50c00890affadccc403c9a3ff0108fe863518
Jakub Gros <kubagros@gmail.com>
2019-10-06 11:26:00 +0200

    added bottom menu

--- src/lunch_it/lib/FoodMenusBar.dart
+++ src/lunch_it/lib/FoodMenusBar.dart
@@ -2,29 +2,22 @@ import 'package:flutter/material.dart';
 
 class FoodMenusBar extends StatelessWidget { //TODO make elements extended if listview is not scrollable
   final _amountOfEntries;
-  final _menuFractionalRelativeSize = 1/15;
 
   FoodMenusBar(this._amountOfEntries);
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: BoxConstraints(minHeight: 20),
-      child: Container(
-        color: Colors.blue[300],
-        child: FractionallySizedBox(
-          heightFactor: _menuFractionalRelativeSize,
-          child: ListView.separated(
-            shrinkWrap: true,
-            separatorBuilder: (context, index) => VerticalDivider(
-              width: 0,
-              color: Colors.black,
-            ),
-            itemCount: _amountOfEntries,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: FoodMenusBarEntryBuilder,
-          ),
+    return Container(
+      color: Colors.blue[300],
+      child: ListView.separated(
+        shrinkWrap: true,
+        separatorBuilder: (context, index) => VerticalDivider(
+          width: 0,
+          color: Colors.black,
         ),
+        itemCount: _amountOfEntries,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: FoodMenusBarEntryBuilder,
       ),
     );
   }
--- src/lunch_it/lib/main.dart
+++ src/lunch_it/lib/main.dart
@@ -36,16 +36,24 @@ class VerticalLayout extends StatelessWidget {
             body: Column(
               children: <Widget>[
                 Expanded(
-                  child: Column(
+                  flex: 1,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Expanded(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            FoodMenusBar(2),
-                          ],
-                        ),
-                      ),
+                      FoodMenusBar(2),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 8,
+                  child: Placeholder() //TODO menu content
+                ),
+                Expanded(
+                  flex:1,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      BottomMenu(),
                     ],
                   ),
                 ),
@@ -53,4 +61,70 @@ class VerticalLayout extends StatelessWidget {
             ) //TODO unmock
             ));
   }
+}
+
+class BottomMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(
+                    child: Text("Food"),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(
+                    child: Text("Food"),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: RaisedButton(
+                    child: Text("Food"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                            "Suma: 100zl",
+                        textAlign: TextAlign.start,),
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text("Pozostalo: 20zl",
+                          textAlign: TextAlign.end,),
+                      ),
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
 }

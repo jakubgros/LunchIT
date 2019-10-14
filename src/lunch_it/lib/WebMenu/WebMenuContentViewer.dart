35016bf458e03c30683b431116bbc484ef2adc3e
Jakub Gros <kubagros@gmail.com>
2019-10-14 19:17:06 +0200

    restructured code of displaying menu

--- src/lunch_it/lib/WebMenu/WebMenuContentViewer.dart
+++ src/lunch_it/lib/WebMenu/WebMenuContentViewer.dart
@@ -25,61 +25,56 @@ class _WebMenuContentViewerState extends State<WebMenuContentViewer> {
 
   Completer<WebViewController> _controller;
 
-  void _goBackCallback()
-  {
-    _controller.future.then((controller){controller.goBack();});
+  void _goBackCallback() {
+    _controller.future.then((controller) {
+      controller.goBack();
+    });
   }
 
-  void _goForwardCallback()
-  {
-    _controller.future.then((controller){controller.goForward();});
+  void _goForwardCallback() {
+    _controller.future.then((controller) {
+      controller.goForward();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        children: <Widget>[
-          WebMenuContentViewerBar(_goBackCallback, _goForwardCallback),
-          Expanded(
-            child: SizedBox(
-              height: 1000,
-              width: 1000,
-              child: StreamBuilder(
-                  stream: _bloc.state,
-                  initialData: MarkModeState.navigateMode(),
-                  builder: (BuildContext context, AsyncSnapshot<MarkModeState>snapshot) {
-
-                    return Stack(
-                      children: <Widget>[
-                        Visibility(
-                          maintainState: true,
-                          visible: snapshot.data.isNavigateMode(),
-                          child: WebView(
-                            initialUrl: _lastUrl,
-                            onPageFinished: (String url){_lastUrl = url;},
-                            onWebViewCreated: (WebViewController webViewController) {
-                              if(_controller.isCompleted == false)
-                                _controller.complete(webViewController);
-                            },),
-                        ),
-                        Coverer(snapshot.data.isNavigateMode() == false),
-                        Visibility(
-                          visible: snapshot.data.isNavigateMode() == false,
-                          child: Placeholder(),
-                        )
-
-
-
-                      ],
-                    );
-                  }
-              ),
-            ),
-          )
-
-        ],
-      ),
+      child: StreamBuilder(
+          stream: _bloc.state,
+          initialData: MarkModeState.navigateMode(),
+          builder:
+              (BuildContext context, AsyncSnapshot<MarkModeState> snapshot) {
+            return Column(
+              children: <Widget>[
+                Flexible(
+                  child: WebMenuContentViewerBar(
+                      _goBackCallback, _goForwardCallback),
+                  flex: 1,
+                ),
+                Flexible(
+                  flex: 14,
+                  child: Stack(
+                    children: <Widget>[
+                      WebView(
+                        initialUrl: widget._webUrl,
+                        onWebViewCreated:
+                            (WebViewController webViewController) {
+                          if (_controller.isCompleted == false)
+                            _controller.complete(webViewController);
+                        },
+                      ),
+                      Coverer(snapshot.data.isNavigateMode() == false),
+                      Visibility(
+                        visible: snapshot.data.isNavigateMode() == false,
+                        child: Placeholder(),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
     );
   }
 
@@ -91,4 +86,3 @@ class _WebMenuContentViewerState extends State<WebMenuContentViewer> {
     _bloc = BlocProvider.of<MarkModeBloc>(context);
   }
 }
-

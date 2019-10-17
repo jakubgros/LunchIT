fbdde589190022d60c46ea798aa8dc7a3e5fc00c
Jakub Gros <kubagros@gmail.com>
2019-10-17 08:36:48 +0200

    code cleanup

--- src/lunch_it/lib/MenuMarker/MenuMarker.dart
+++ src/lunch_it/lib/MenuMarker/MenuMarker.dart
@@ -22,11 +22,6 @@ class MenuMarker extends StatefulWidget {
 }
 
 class _MenuMarkerState extends State<MenuMarker> {
-  double _startX = 0;
-  double _startY = 0;
-  double _endX = 0;
-  double _endY = 0;
-
   Offset _start;
   Offset _end;
 
@@ -34,25 +29,14 @@ class _MenuMarkerState extends State<MenuMarker> {
   ScreenshotController _screenshotController;
 
   void gesturePanStartCallback(DragStartDetails details) {
-    var localPos = details.localPosition;
-
-
     setState((){
       _start = details.localPosition;
-
-      _startX = localPos.dx;
-      _startY = localPos.dy;
     });
   }
 
   void gesturePanUpdateCallback(DragUpdateDetails details) {
-    var localPos = details.localPosition;
-
     setState(() {
       _end = details.localPosition;
-
-      _endX = localPos.dx;
-      _endY = localPos.dy;
     });
   }
 
@@ -98,7 +82,6 @@ class _MenuMarkerState extends State<MenuMarker> {
 
 
 class Marker extends StatelessWidget {
-
   Rect _rect;
 
   Marker(Offset start, Offset end) {
@@ -122,47 +105,3 @@ class Marker extends StatelessWidget {
     );
   }
 }
-
-
-
-/*
-class Marker extends StatelessWidget {
-  AxisDirection _horizontalDir;
-  AxisDirection _verticalDir;
-  double _width;
-  double _height;
-  double _x;
-  double _y;
-  Container _container;
-
-  Marker(this._horizontalDir, this._verticalDir, this._width, this._height,
-      this._x, this._y, Color color) {
-    _container = Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: color),
-      ),
-      child: FittedBox(
-        fit: BoxFit.cover,
-        child: SizedBox(
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if(_horizontalDir == AxisDirection.up && _verticalDir == AxisDirection.left){
-
-    } else if(_horizontalDir == AxisDirection.up && _verticalDir == AxisDirection.right){
-
-    } else if(_horizontalDir == AxisDirection.down && _verticalDir == AxisDirection.left){
-
-    } else if(_horizontalDir == AxisDirection.down && _verticalDir == AxisDirection.right){
-
-    }
-
-
-  }
-}
-*/
-

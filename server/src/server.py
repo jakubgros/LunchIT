a731f9910a413d6f56ccefe2cc2ad00f70b977ad
Jakub Gros <kubagros@gmail.com>
2019-11-10 20:17:52 +0100

    added page that displays order requests, adjusted server for cors

--- server/src/server.py
+++ server/src/server.py
@@ -1,9 +1,11 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 from src.endpoints import *
 from src.utils.config import get_config
 
-WEB_CLIENT_PATH = "C:/Users/jakub/Desktop/LunchIT/web_client" #TODO dehardcode
+
 app = Flask(__name__)
+
+
 
 config = get_config("config.ini", "mail")
 config["MAIL_PORT"]= int(config["MAIL_PORT"])
@@ -13,10 +15,11 @@ app.config.update(config)
 
 app.register_blueprint(routes)
 
-@app.route('/lunch_it/<string:page_name>/') #TODO change all endpoints to start with /api/ and this to not contain /lunch_it/
-def send_static(page_name):
-    file_name = '%s.html' % page_name
-    return send_from_directory(WEB_CLIENT_PATH, file_name)
+@app.after_request # TODO adjust on production version
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, port='5002')

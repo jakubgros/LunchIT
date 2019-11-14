8be7d68f186bd69a185b9db31da193ee2f25f5c0
Jakub Gros <kubagros@gmail.com>
2019-11-14 20:11:50 +0100

    LN-72 removed not needed try catch blocks

--- mobile_client/lib/ServerApi/ServerApi.dart
+++ mobile_client/lib/ServerApi/ServerApi.dart
@@ -20,37 +20,11 @@ enum Method {
 
 class ServerApi
 {
-  String _email;
-  String _hashedPassword;
-
-  static String _serverAdress = "10.0.2.2:5002";
-  final http.Client _client = http.Client();
-
-  ServerApi._privateCtor();
-  static final ServerApi _singleton = ServerApi._privateCtor();
-  factory ServerApi() => _singleton;
-
-  Future<String> get _rememberedCredentialsFilePath async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String credFileName = "cred.txt";
-    return appDocDir.path + "/" + credFileName;
-  }
-  
-  void closeConnection() { //TODO call it somewhere
-    _client.close();
-  }
-
-  Map<String, String> _getAuthHeader() {
-    var authHeader = Map<String, String>.from({'authorization': "$_email:$_hashedPassword"});
-    return authHeader;
-  }
-
   Future<String> getAsText(File imageFile) => compute(_getAsText, imageFile);
   static Future<String> _getAsText(File imageFile) async {
     String endpoint = "/getAsText";
     String method = "POST";
     // ==============================
-
 
     var stream = http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
     var length = await imageFile.length();
@@ -69,6 +43,144 @@ class ServerApi
     String result = await response.stream.transform(utf8.decoder).elementAt(0);
 
     return result;
+  }
+
+  Future<bool> placeOrder(Order order) async {
+    http.Response response = await _sendJsonRequest(
+      endpoint: '/order',
+      method: Method.POST,
+      body: jsonEncode(order),
+      sendWithAuthHeader: true,
+    );
+
+    return response.statusCode == 200;
+  }
+
+  Future<bool> logIn(String email, String password, { bool isPasswordHashed=false,
+    bool rememberCredentials }) async {
+
+    if(isPasswordHashed == false)
+      password = _hash(password);
+
+    http.Response response = await _sendJsonRequest(
+      endpoint: '/authenticate',
+      method: Method.POST,
+      sendWithAuthHeader: false,
+      body: jsonEncode({
+        "user_id": email,
+        "hashed_password": password,
+      }),
+    );
+
+    final responseJson = json.decode(response.body);
+
+    bool hasLoggedIn = responseJson['authenticated'] == true;
+
+    if(hasLoggedIn) { // save for future calls
+      _email = email;
+      _hashedPassword = password;
+    }
+
+    if(rememberCredentials)
+      _rememberCredentials(email, password);
+
+    return hasLoggedIn;
+  }
+
+  Future<List<OrderRequest>> getOrderRequestsForCurrentUser() async{
+    http.Response response = await _sendJsonRequest(
+      endpoint: '/orderRequestForSingleUser',
+      method: Method.GET,
+      sendWithAuthHeader: true,
+    );
+
+    if(response.statusCode != 200) //TODO extract statusCode processing to seperate method
+      throw Exception("error");
+
+    var orderRequests = List<OrderRequest>();
+    var responseDecoded = jsonDecode(response.body);
+    for(Map parsedJsonObj in responseDecoded)
+      orderRequests.add(OrderRequest.fromJsonMap(parsedJsonObj));
+
+    return orderRequests;
+  }
+
+  Future<bool> checkSavedCredentials() async {
+    bool hasLoadedSuccessfully = await _loadSavedCredentials();
+    if(hasLoadedSuccessfully == false)
+      return false;
+    return logIn(_email, _hashedPassword, isPasswordHashed: true, rememberCredentials: false);
+  }
+
+  Future<List<BasketEntry>> getPlacedOrder(int id) async {
+    http.Response response = await _sendJsonRequest(
+      endpoint: '/order',
+      method: Method.GET,
+      queryParameters: {
+        "placed_order_id": id.toString()
+      },
+      sendWithAuthHeader: true,
+    );
+
+    if(response.statusCode != 200) //TODO extract statusCode processing to seperate method
+      throw Exception("error");
+
+    var order = List<BasketEntry>();
+    List listOfJsonObj = jsonDecode(response.body);
+    for(Map jsonObj in listOfJsonObj)
+      order.add(BasketEntry.fromJsonMap(jsonObj));
+
+    return order;
+  }
+
+  void logout() {
+    _email = "";
+    _hashedPassword = "";
+    _removeSavedCredentials();
+  }
+
+  Future<bool> registerUser(String email, String password) async {
+    http.Response response = await _sendJsonRequest(
+        endpoint: '/create_account',
+        method: Method.GET,
+        sendWithAuthHeader: true,
+        body: jsonEncode({
+          "user_id": email,
+          "hashed_password": _hash(password),
+        })
+    );
+
+    final responseJson = json.decode(response.body);
+    bool hasCreatedUser = responseJson['status'] == "created";
+
+    return hasCreatedUser;
+  }
+
+  void closeConnection() { //TODO call it somewhere
+    _client.close();
+  }
+
+  factory ServerApi() => _singleton;
+
+  // private methods and fields
+
+  String _email;
+  String _hashedPassword;
+  static String _serverAdress = "10.0.2.2:5002";
+  final http.Client _client = http.Client();
+
+  ServerApi._privateCtor();
+  static final ServerApi _singleton = ServerApi._privateCtor();
+
+  Future<String> get _rememberedCredentialsFilePath async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String credFileName = "cred.txt";
+    return appDocDir.path + "/" + credFileName;
+  }
+
+  Map<String, String> _getAuthHeader() {
+    var authHeader = Map<String, String>.from({'authorization': "$_email:$_hashedPassword"});
+    return authHeader;
   }
 
   Future<http.Response> _sendJsonRequest(
@@ -108,107 +220,7 @@ class ServerApi
     return response;
   }
 
-  Future<bool> placeOrder(Order order) async { //TODO make it work on seperate isolate
-
-    http.Response response = await _sendJsonRequest(
-        endpoint: '/order',
-        method: Method.POST,
-        body: jsonEncode(order),
-        sendWithAuthHeader: true,
-    );
-
-    return response.statusCode == 200;
-  }
-
-
-  Future<bool> logIn(String email, String password, { bool isPasswordHashed=false,
-        bool rememberCredentials }) async {
-
-    if(isPasswordHashed == false)
-      password = _hash(password);
-
-    http.Response response = await _sendJsonRequest(
-      endpoint: '/authenticate',
-      method: Method.POST,
-      sendWithAuthHeader: false,
-      body: jsonEncode({
-        "user_id": email,
-        "hashed_password": password,
-      }),
-    );
-
-    final responseJson = json.decode(response.body);
-
-    bool hasLoggedIn = responseJson['authenticated'] == true;
-
-    if(hasLoggedIn) { // save for future calls
-      _email = email;
-      _hashedPassword = password;
-    }
-
-    if(rememberCredentials)
-      _rememberCredentials(email, password);
-
-    return hasLoggedIn;
-  }
-
   static String _hash(String password) => Password.hash(password, PBKDF2());
-
-  Future<List<OrderRequest>> getOrderRequestsForCurrentUser() async{
-    try{
-      http.Response response = await _sendJsonRequest(
-        endpoint: '/orderRequestForSingleUser',
-        method: Method.GET,
-        sendWithAuthHeader: true,
-      );
-
-      if(response.statusCode != 200) //TODO extract statusCode processing to seperate method
-        throw Exception("error");
-
-      var orderRequests = List<OrderRequest>();
-      var responseDecoded = jsonDecode(response.body);
-      for(Map parsedJsonObj in responseDecoded)
-        orderRequests.add(OrderRequest.fromJsonMap(parsedJsonObj));
-
-      return orderRequests;
-    }
-    catch(e) {
-      print(e); //TODO add try catch to all async because it gets swallowed otherwise
-    }
-  }
-
-  Future<bool> checkSavedCredentials() async {
-    bool hasLoadedSuccessfully = await _loadSavedCredentials();
-    if(hasLoadedSuccessfully == false)
-      return false;
-    return logIn(_email, _hashedPassword, isPasswordHashed: true, rememberCredentials: false);
-  }
-
-  Future<List<BasketEntry>> getPlacedOrder(int id) async {
-    try{
-      http.Response response = await _sendJsonRequest(
-        endpoint: '/order',
-        method: Method.GET,
-        queryParameters: {
-          "placed_order_id": id.toString()
-        },
-        sendWithAuthHeader: true,
-      );
-
-      if(response.statusCode != 200) //TODO extract statusCode processing to seperate method
-        throw Exception("error");
-
-      var order = List<BasketEntry>();
-      List listOfJsonObj = jsonDecode(response.body);
-      for(Map jsonObj in listOfJsonObj)
-        order.add(BasketEntry.fromJsonMap(jsonObj));
-
-      return order;
-    }
-    catch(e){
-      print(e); //TODO add try catch to all async because it gets swallowed otherwise
-    }
-  }
 
   void _rememberCredentials(String email, String hashedPassword) async {
     File credFile = await File(await _rememberedCredentialsFilePath).create();
@@ -231,27 +243,4 @@ class ServerApi
   }
 
   void _removeSavedCredentials() => _rememberCredentials("","");
-
-  void logout() {
-    _email = "";
-    _hashedPassword = "";
-    _removeSavedCredentials();
-  }
-
-  Future<bool> registerUser(String email, String password) async {
-    http.Response response = await _sendJsonRequest(
-      endpoint: '/create_account',
-      method: Method.GET,
-      sendWithAuthHeader: true,
-      body: jsonEncode({
-            "user_id": email,
-            "hashed_password": _hash(password),
-      })
-    );
-
-    final responseJson = json.decode(response.body);
-    bool hasCreatedUser = responseJson['status'] == "created";
-
-    return hasCreatedUser;
-  }
 }
